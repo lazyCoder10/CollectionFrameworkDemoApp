7da1dcb687365d16f8379f864d3676da484d51d9
rahma <rahma@DESKTOP-LOD1LHN>
2017-04-21 20:36:17 +0600

    #Hash set demo

--- src/com/collectionFramework/main/Main.java
+++ src/com/collectionFramework/main/Main.java
@@ -1,12 +1,21 @@
 package com.collectionFramework.main;
 
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
-		System.out.println("hello");
-		System.out.println("hello");
+		Set<String> addWords = new HashSet<String>();
+		addWords.add("I");
+		addWords.add("came");
+		addWords.add("I");
+		addWords.add("Win");
+		System.out.print(addWords.size()+" disticnt element: ");
+		for(String s : addWords){
+			System.out.print(s+" ");
+		}
+		
 
 	}
 
