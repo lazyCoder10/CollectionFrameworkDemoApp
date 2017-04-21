c3950139d88796db3c3672457badcf1adfaeb75f
rahma <rahma@DESKTOP-LOD1LHN>
2017-04-21 14:55:21 +0600

    initial commit

---
+++ src/com/collectionFramework/main/Main.java
@@ -0,0 +1,12 @@
+package com.collectionFramework.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		
+
+	}
+
+}
